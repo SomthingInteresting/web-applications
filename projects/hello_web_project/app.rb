628e342e425ab71b4ee741fe013fde6ee96223f4
Quddus Rahman <qrahman115@gmail.com>
2023-04-03 17:39:51 +0100

    completed up to 02 building a route

---
+++ projects/hello_web_project/app.rb
@@ -0,0 +1,40 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    "Hello World"
+  end
+
+  post '/goodbye' do
+    name = params[:name] # The value is 'Alice'
+  
+    # Do something with `name`...
+  
+    return "Hello #{name}"
+  end
+
+  get '/hello' do
+    name = params[:name] # The value is 'Alice'
+  
+    # Do something with `name`...
+  
+    return "Hello #{name}"
+  end
+
+  post '/submit' do
+    name = params[:name] # The value is 'Alice'
+    message = params[:message] # The value is 'Hello World'
+  
+    # Do something with `name`...
+  
+    return "Thanks #{name}, you sent this message: #{message}"
+  end
+
+end
